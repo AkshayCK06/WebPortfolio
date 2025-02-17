811a3066d966e6106b35b1b3e772fb103e67a0fd
Akshay C Kashyap <akshayckashyap@gmail.com>
2025-02-17 16:10:44 +0530

    added the responsive, animations, smooth scroling and nav bar

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
 document.addEventListener("DOMContentLoaded", function() {
     console.log("Portfolio Loaded!");
 });
+
+document.querySelectorAll('nav ul li a').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
+AOS.init();
